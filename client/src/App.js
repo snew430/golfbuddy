b9e5a11cdccf47f601f3c94c748d0ebade7655d0
ckyle121 <carolinekyle21@gmail.com>
2022-05-13 12:27:25 -0500

    rules styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import './App.scss';
 function App() {
   return (
     <div className="app">
-      <Nav />
+      {/* <Nav /> */}
       <Tournament />
       <Rules />
       <History />
--- client/src/components/Rules/Rules.jsx
+++ client/src/components/Rules/Rules.jsx
@@ -1,40 +1,36 @@
 import React from 'react';
+import './Rules.scss'
 
 const Rules = () => {
   return (
-    <div>
-        <h2 className='head-text'>Rules & Regulations</h2>
+    <div id='rules' className='app__darkgreenbg'>
+        <h2 className='head-text-purple'>Rules & Regulations</h2>
 
-        <p>
-            Welcome to the guys joining us for golf for the first time! Here's
-            information we hope will make your golfing a bit more enjoyable. Each day
-            we have competitive games. All competition is based on what you shoot
+        <p className='p-text'>
+            Welcome to the guys joining us for the first time! <br/> Here's
+            information that will make your golfing more enjoyable. All competition is based on what you shoot
             against your own the average score (or what you told us you shoot).
-        </p>
-
-        <p>
-            Each day has prize money, so make sure to bring your A game. 
-            You have already anteed up when you paid for your golf.
+            Each day has prize money, so make sure to bring your A game. You have already anteed up when you paid for your golf.
         </p>
 
         <ul>
             <li>Senior Tees
-                <p>We are all using the tees that are 5800 yards and shorter</p>
+                <p className='p-text'>We are all using the tees that are 5800 yards and shorter</p>
             </li>
             <li> Max stroke on any hole is Par plus 3
-                <p>On a par 3, max is 6, par 4 max is 7, par 5 max is 8. Once you reach this score, please pick up and move to the next hole.</p>
+                <p className='p-text'>On a par 3, max is 6, par 4 max is 7, par 5 max is 8. Once you reach this score, please pick up and move to the next hole.</p>
             </li>
             <li> Balls hit OB, into the water, or any other hazard
-                <p>Play the ball laterally and add a stroke (if in the water drop on other side). Provisional balls not necessary.</p>
-                <p>For unplayable sand traps, ok to move the ball to where it can be played, but it cannot be closer to the pin</p>
-                <p>Ok to roll the ball in the fairway, lift and clean is ok.</p>
+                <p className='p-text'>Play the ball laterally and add a stroke (if in the water drop on other side). Provisional balls not necessary.</p>
+                <p className='p-text'>For unplayable sand traps, ok to move the ball to where it can be played, but it cannot be closer to the pin</p>
+                <p className='p-text'>Ok to roll the ball in the fairway, lift and clean is ok.</p>
             </li>
             <li>Putts: Gimmes are Ok
-                <p>Player must add a stroke for the gimme.</p>
+                <p className='p-text'>Player must add a stroke for the gimme.</p>
             </li>
         </ul>
 
-        <p>Please reach out to us if you have questions. <br />Looking forward to a great trip!</p>
+        <h3>Please reach out to us if you have questions. <br />Looking forward to a great trip!</h3>
     </div>
   );
 }
