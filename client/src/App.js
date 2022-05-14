4ecabeddd8d0789fd5961ed3f1dc1741303df283
snew430 <seanpnew@hotmail.com>
2022-05-14 13:46:06 -0400

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,51 @@
 import React from "react";
-import { Rules, History, Nav, Tournament, SignUp } from './components';
-import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Rules, History, Tournament } from "./pages";
+import { Nav } from "./components";
+import "./App.scss";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="app">
-      {/* <Nav /> */}
-      <Tournament />
-      <Rules />
-      <History />
-      <SignUp />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="app">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Tournament />} />
+            <Route path="/tournament" element={<Tournament />} />
+            <Route path="/rules" element={<Rules />} />
+            <Route path="/history" element={<History />} />
+            {/* <Route path="*" element={<NoMatch />} /> */}
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/Rules/Rules.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Rules = () => {
+  return (
+    <div>
+        <h2 className='head-text'>Rules & Regulations</h2>
+
+        <p>
+            Welcome to the guys joining us for golf for the first time! Here's
+            information we hope will make your golfing a bit more enjoyable. Each day
+            we have competitive games. All competition is based on what you shoot
+            against your own the average score (or what you told us you shoot).
+        </p>
+
+        <p>
+            Each day has prize money, so make sure to bring your A game. 
+            You have already anteed up when you paid for your golf.
+        </p>
+
+        <ul>
+            <li>Senior Tees
+                <p>We are all using the tees that are 5800 yards and shorter</p>
+            </li>
+            <li> Max stroke on any hole is Par plus 3
+                <p>On a par 3, max is 6, par 4 max is 7, par 5 max is 8. Once you reach this score, please pick up and move to the next hole.</p>
+            </li>
+            <li> Balls hit OB, into the water, or any other hazard
+                <p>Play the ball laterally and add a stroke (if in the water drop on other side). Provisional balls not necessary.</p>
+                <p>For unplayable sand traps, ok to move the ball to where it can be played, but it cannot be closer to the pin</p>
+                <p>Ok to roll the ball in the fairway, lift and clean is ok.</p>
+            </li>
+            <li>Putts: Gimmes are Ok
+                <p>Player must add a stroke for the gimme.</p>
+            </li>
+        </ul>
+
+        <p>Please reach out to us if you have questions. <br />Looking forward to a great trip!</p>
+    </div>
+  );
+}
+
+export default Rules;
