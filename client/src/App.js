c0716579f5a0a521f77acaad162b77fc157e236f
ckyle121 <carolinekyle21@gmail.com>
2022-05-17 18:17:38 -0500

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { Rules, History, Tournament, SignUp, Home, Administration, PlayerList } from "./pages";
+import { Rules, History, Tournament, SignUp, Home, Administration } from "./pages";
 import { Nav } from "./components";
 import "./App.scss";
 
@@ -34,7 +34,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/tournament" element={<Tournament />} />
-            <Route path="/rules" element={<PlayerList />} />
+            <Route path="/rules" element={<Rules />} />
             <Route path="/history" element={<History />} />
             <Route path="/administration" element={<Administration />} />
             {/* <Route path="*" element={<NoMatch />} /> */}
