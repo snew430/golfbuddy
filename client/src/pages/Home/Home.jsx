8eb1c7de796c471e56450c4746b3a31958787067
ckyle121 <carolinekyle21@gmail.com>
2022-05-18 16:57:53 -0500

    change all tournament names to trip and update navbar

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,9 +4,9 @@ import './Home.scss';
 const Home = () => {
   return (
     <div id='home'>
-        <h2 className='head-text-home'>Early Birdie Golf Tournaments</h2>
+        <h2 className='head-text-home'>Early Birdie Golf Trips</h2>
         <div className='app__flex'>
-            <button>Sign Up for the Next Tournament</button>
+            <button>Sign Up for the Next Trip</button>
         </div>
     </div>
   );
