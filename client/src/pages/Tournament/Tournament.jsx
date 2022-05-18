91c879082cfc47e0bd206e74ddade15f3ee52f3e
ckyle121 <carolinekyle21@gmail.com>
2022-05-18 14:47:18 -0500

    styling edits

--- client/src/pages/Tournament/Tournament.jsx
+++ client/src/pages/Tournament/Tournament.jsx
@@ -4,34 +4,30 @@ import './Tournament.scss';
 const Tournament = () => {
   return (
     <div id='tournament'>
-      <h2>Trip Name</h2>
-      <h3>Dates</h3>
-
-      <div className='app__flex'>
-        <button>Sign Up for this Trip</button>
-      </div>
-
-      <div className='trip-details'>
-
-        <div className='accomodations'>
-          <h4>We are staying at HOTEL</h4>
-          <a href="">link to hotel website</a>
-            <p>Price for Single Room: </p>
-            <p>Price for Double Room: </p>
-            <p>Golf Only Price: </p>
+      <div className='background'>
+        <h2>Trip Name</h2>
+        <h3>Dates</h3>
+        <div className='app__flex'>
+          <button>Sign Up for this Trip</button>
         </div>
-
-        <div className='course-details'>
-            <p>Day One Course: </p>
-            <p>Day Two Course: </p>
-            <p>Day Three Course: </p>
-            <p>Day Four Course: </p>
+        <div className='trip-details'>
+          <div className='accomodations'>
+            <h4>We are staying at HOTEL</h4>
+            <a href="">link to hotel website</a>
+              <p>Price for Single Room: </p>
+              <p>Price for Double Room: </p>
+              <p>Golf Only Price: </p>
+          </div>
+          <div className='course-details'>
+              <p>Day One Course: </p>
+              <p>Day Two Course: </p>
+              <p>Day Three Course: </p>
+              <p>Day Four Course: </p>
+          </div>
+          <p>All payments are due: DUEDATE < br/>
+            Please send payments to
+          </p>
         </div>
-
-        <p>All payments are due: DUEDATE < br/>
-          Please send payments to 
-        </p>
-
       </div>
 
     </div>
