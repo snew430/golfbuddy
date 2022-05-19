695b325793df003425510a8b34bd7e1c7f0cb86f
ckyle121 <carolinekyle21@gmail.com>
2022-05-18 20:13:30 -0500

    add first mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,12 @@
 import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
