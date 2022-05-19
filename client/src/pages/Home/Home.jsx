4dfa301b63240ca57d0b416b9bc26a693061cd2f
ckyle121 <carolinekyle21@gmail.com>
2022-05-18 19:11:00 -0500

    add link for admin

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.scss';
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
--- client/src/pages/Tournament/Tournament.jsx
+++ client/src/pages/Tournament/Tournament.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Tournament.scss';
+import { Link } from "react-router-dom";
 
 const Tournament = () => {
   return (
