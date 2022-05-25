8dbfe1dd3b0fd5a6154e1540d649d10dc9cc0484
snew430 <seanpnew@hotmail.com>
2022-05-24 21:10:50 -0400

    updated

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -11,6 +11,7 @@ import Modal from "../../components/Modal/Modal";
 import Auth from "../../utils/auth";
 
 const List = ({ players, status, tournament, refetchPlayers }) => {
+  console.log(players);
   const [currentPlayer, setCurrentPlayer] = useState();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
