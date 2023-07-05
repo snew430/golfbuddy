12a1f8534f0b6a8ced227f6e38ce2a028b989ae9
ckyle121 <carolinekyle21@gmail.com>
2023-07-05 18:47:35 -0500

    cheat page styling

--- client/src/components/Cheat/Cheat.jsx
+++ client/src/components/Cheat/Cheat.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Cheat.scss';
 
 const Cheat = () => {
   return (
-    <div className="cheat-container">
-      <h3 className="cheat-text">
-        You need to log in first. Don't cheat by looking at something you're not
-        supposed to. <br />
-        Makes me think you cheat at golf too
-      </h3>
+    <div className="cheat-container" id="cheat">
+      <div className='background'>
+        <p className="p-text">
+          You need to log in first. 
+          <br /> 
+          Don't cheat by trying something you're not
+          supposed to. 
+          <br />
+          Makes me think you cheat at golf too...
+        </p>
+      </div>
     </div>
   );
 }
