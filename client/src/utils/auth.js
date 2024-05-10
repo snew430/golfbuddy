1126c17895137024779ce583a5d492cc8b3b6793
snew430 <seanpnew@hotmail.com>
2024-05-09 21:30:18 -0400

    updated email and error handling

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -19,7 +19,7 @@ class AuthService {
       !!token &&
       !this.isTokenExpired(token) &&
       admin.data.email === 'whatsmyteetime@outlook.com'
-    ); // handwaiving here
+    );
   }
 
   isTokenExpired(token) {
