ff0da514b6cf3b2112a3dd18427fca34b2eae447
snew430 <seanpnew@hotmail.com>
2024-10-08 09:16:56 -0400

    Updates for fall 2024

--- client/src/assets/files/index.js
+++ client/src/assets/files/index.js
@@ -1,5 +1,5 @@
-import monday4Somes from './MondayFoursomesSpring2024.docx';
-import tuesday4Somes from './TuesdayFoursomesSpring2024.docx';
-import wednesday4Somes from "./WednesdayFoursomesSpring2024.docx";
+import monday4Somes from './MondayFoursomes.docx';
+import tuesday4Somes from './TuesdayFoursomes.docx';
+// import wednesday4Somes from "./WednesdayFoursomes.docx";
 
-export {monday4Somes, tuesday4Somes, wednesday4Somes};
+export {monday4Somes, tuesday4Somes};
